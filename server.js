1ec541a09251daad322f6acdfdb551c1dd2b1852
Khushil Mistry <khushil324@gmail.com>
2017-12-20 19:49:54 +0530

    Added a Procfile.

--- server.js
+++ server.js
@@ -92,8 +92,6 @@ app.get('/login/facebook',
 app.get('/login/facebook/return',
   passport.authenticate('github'),
   function(req, res) {
-    console.log(req.user);
-    res.json({ user: req.user });
     res.redirect('http://localhost:3000/Github');
   });
 
